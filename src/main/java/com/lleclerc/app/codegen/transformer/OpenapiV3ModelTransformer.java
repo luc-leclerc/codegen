96edac515f4f087c8bbb5b20472a949032b7e55f
luc-leclerc <leclerc.luc@gmail.com>
2021-06-18 08:44:35 -0400

    wip

---
+++ src/main/java/com/lleclerc/app/codegen/transformer/OpenapiV3ModelTransformer.java
@@ -0,0 +1,13 @@
+package com.lleclerc.app.codegen.transformer;
+
+import com.lleclerc.service.swagger.OpenapiV3Model;
+
+import java.util.List;
+
+public interface OpenapiV3ModelTransformer {
+
+    static List<Object> transform(OpenapiV3Model openapi) {
+//        openapi.getComponents().getSchemas().
+        return null;
+    }
+}
---
+++ src/test/java/com/lleclerc/app/codegen/transformer/OpenapiV3ModelTransformerTest.java
@@ -0,0 +1,22 @@
+package com.lleclerc.app.codegen.transformer;
+
+import com.lleclerc.service.java.ResourceUtil;
+import com.lleclerc.service.swagger.OpenapiV3Model;
+import com.lleclerc.service.yml.YamlUtil;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class OpenapiV3ModelTransformerTest implements ResourceUtil {
+    @Test
+    public void transform() {
+        // GIVEN
+        OpenapiV3Model model = YamlUtil.parseYaml_jackson(readResource("swagger-pet-sample.yml"), OpenapiV3Model.class);
+
+        // WHEN
+        OpenapiV3ModelTransformer.transform(model);
+
+        // THEN
+        assertEquals("3.0.0", model.getOpenapi());
+    }
+}
